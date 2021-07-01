fb6939858df54935a70423cee5ee455ddee2b94f
Your Name <takialog@gmail.com>
2021-07-01 11:06:03 +0800

    setup eslint tsconfig test

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+
+/**
+ * Calculate the average of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @public
+ */
+ export function avg(a: number, b: number, c: number): number {
+  return sum3(a, b, c) / 3;
+}
+
+/**
+ * Calculate the sum of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @beta
+ */
+export function sum3(a: number, b: number, c: number): number {
+  return sum2(a, sum2(b, c));
+}
+
+/**
+ * Calculate the sum of two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @internal
+ * type of sum2 is not part of type information
+ */
+export function sum2(a: number, b: number): number {
+  const sum = a + b;
+  return sum;
+}
